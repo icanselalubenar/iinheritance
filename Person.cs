f85af1ad958895bc462153f023550ef6720f3795
Ikhsan Abdul Rafi <107069762+icanselalubenar@users.noreply.github.com>
2023-06-05 17:23:08 +0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace iinheritance
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Name = "[belum diatur]";
+            Age = 0;
+        }
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public void InfoPerson()
+        {
+            Console.WriteLine("Nama saya {0}, dan umur saya {1} tahun.\n", Name, Age);
+        }
+    }
+
+    public class karyawan : Person
+    {
+        public string KaryawanID { get; set; }
+        public string subject { get; set; }
+
+        public karyawan()
+        {
+            Name = "[belum diatur]";
+            Age = 0;
+        }
+        public karyawan(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public void InfoPerson(string subject, string KaryawanID)
+        {
+            this.subject = subject;
+            this.KaryawanID = KaryawanID;
+
+            Console.WriteLine("Saya disini sebagai {0}, dengan ID {1}", this.subject, this.KaryawanID);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace iinheritance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person orang01 = new Person();
+            Person orang02 = new Person("Ikhsan", 19);
+            karyawan karyawan01 = new karyawan("Zahra", 20);
+
+            orang01.InfoPerson();
+            orang02.InfoPerson();
+
+            karyawan01.InfoPerson();
+            karyawan01.InfoPerson("Kowad", "001122");
+            Console.ReadKey();
+        }
+    }
+}
